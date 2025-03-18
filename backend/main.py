22480020d3217b31d9fa61a9df55f6d3567fc2da
Calvin <81276822+calvintvu@users.noreply.github.com>
2025-03-17 21:10:42 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -45,17 +45,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://localhost:10000",
-    "https://autosampler.onrender.com",
-    "*"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
